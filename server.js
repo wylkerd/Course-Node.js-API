5d4b57995a462d4884c86c2ea8af745560bb47a4
Wylkerd <wylkerd@hotmail.com>
2020-11-25 12:07:17 -0300

    Creating model of Product

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser:true });
+mongoose.connect(
+ 'mongodb://localhost:27017/nodeapi',
+ { useUnifiedTopology: true, useNewUrlParser:true });
+
+// Registro do MOdel Product
+require('./src/models/Product');
 
 // Primeira rota
 app.get('/',(req, res) => {
