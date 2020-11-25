f59b01f1c92aaa30be05956f45e3ff27c64e81a7
Wylkerd <wylkerd@hotmail.com>
2020-11-25 20:16:50 -0300

    Adding new datas by Me

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,6 +4,16 @@ const Product = mongoose.model('Product');
 
 // exportando objeto com algumas funções
 module.exports = {
+    async insert(req, res) {
+        const insert = await Product.create({
+            title: 'Heloisy',
+            description: 'Minha irmã',
+            url: "####2"
+        })
+
+        return res.json(insert);
+    },
+    
     async index(req, res) {
         const products = await Product.find();
 
--- src/models/Routes.js
+++ src/models/Routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const ProductController = require('../controllers/ProductController');
 
+routes.put(ProductController.insert);
 routes.get('/products', ProductController.index );
 
- module.exports = routes;
+module.exports = routes;
