d01d0d735473258792e5605b29c51b287d76f00e
Wylkerd <wylkerd@hotmail.com>
2020-11-25 21:15:43 -0300

    Adding CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
@@ -6,6 +7,7 @@ const requireDir = require('require-dir');
 const app = express();
 // Permitindo enviar informação para este app, em formato JSON
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect(
